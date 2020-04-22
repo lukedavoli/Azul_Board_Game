dfb81060b76af0dd6bc9c6f1e75e85d9f424c2f4
lukedavoli <s3782747@student.rmit.edu.au>
2020-04-22 16:12:53 +1000

    main and menu implemented

---
+++ Menu.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include "Menu.h"
+
+void Menu::displayWelcome()
+{
+    std::cout << "Welcome to Azul!\n" <<
+                 MEDIUM_LB <<
+                 std::endl;
+}
+
+void Menu::displayMenu()
+{
+    bool quit = false;
+    while(!quit)
+    {
+        std::cout << "Menu\n" <<
+                  SHORT_LB << "\n" <<
+                  "1. New Game\n" <<
+                  "2. Load Game\n" <<
+                  "3. Credits\n" <<
+                  "4. Quit\n" <<
+                  std::endl;
+
+        int menuSelection = -1;
+
+        std::cout << PROMPT;
+        std::cin >> menuSelection;
+        std::cout << "\n";
+
+        if(menuSelection == 1)
+        {
+            newGame();
+        }
+        else if(menuSelection == 2)
+        {
+            loadGame();
+        }
+        else if(menuSelection == 3)
+        {
+            showCredits();
+        }
+        else if(menuSelection == 4)
+        {
+            quit = true;
+            std::cout << "Goodbye" << std::endl;
+        }
+        else
+        {
+            std::cout << "Invalid option, please try again.";
+        }
+    }
+}
+
+void Menu::showCredits()
+{
+    std::cout << LONG_LB << std::endl;
+    for(int i = 0; i < STUDENTS; i++)
+    {
+        std::cout << "Name: " << names[i] << std::endl;
+        std::cout << "Student ID: " << ids[i] << std::endl;
+        std::cout << "Email: " << emails[i] << std::endl;
+
+        if(i != STUDENTS - 1)
+        {
+            std::cout << "\n";
+        }
+    }
+    std::cout << LONG_LB << "\n" << std::endl;
+}
+
+void Menu::newGame()
+{
+    std::string p1Name = "";
+    std::string p2Name = "";
+
+    std::cout << "Starting a New Game\n" << std::endl;
+    std::cout << "Enter a name for player 1" << std::endl;
+    std::cout << PROMPT;
+    std::cin >> p1Name;
+    std::cout << "Enter a name for player 2" << std::endl;
+    std::cout << PROMPT;
+    std::cin >> p2Name;
+
+    std::cout << "\nLet's Play!\n" << std::endl;
+
+    //TODO start game
+}
+
+void Menu::loadGame()
+{
+    std::string filename;
+
+    std::cout << "Enter the filename from which to load the game" << std::endl;
+    std::cout << PROMPT;
+    std::cin >> filename;
+
+    //TODO check that file exists
+    //TODO check that file contains a valid game (format of valid game TBC)
+    //TODO load game and continue
+}
+
+
+
---
+++ Menu.h
@@ -0,0 +1,49 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include <string>
+
+#define SHORT_LB "----"
+#define MEDIUM_LB "-------------------"
+#define LONG_LB "----------------------------------"
+
+#define PROMPT "> "
+
+#define STUDENTS 3
+#define S1_NAME  "Michael Lyberopoulos"
+#define S2_NAME  "Nikita Phung"
+#define S3_NAME  "Luke Davoli"
+#define S1_SID   "s3787260"
+#define S2_SID   "s3783287"
+#define S3_SID   "s3782747"
+#define S1_EMAIL "s3787260@student.rmit.edu.au"
+#define S2_EMAIL "s3783287@student.rmit.edu.au"
+#define S3_EMAIL "s3782747@student.rmit.edu.au"
+
+
+
+class Menu{
+public:
+    //Welcome message to be displayed once at the beginning of the program
+    void displayWelcome();
+
+    //Opens the menu
+    void displayMenu();
+
+    //Displays students name, id and email
+    void showCredits();
+
+private:
+    std::string names[STUDENTS] = {S1_NAME, S2_NAME, S3_NAME};
+    std::string ids[STUDENTS] = {S1_SID, S2_SID, S3_SID};
+    std::string emails[STUDENTS] = {S1_EMAIL, S2_EMAIL, S3_EMAIL};
+
+    //Collects player names before starting a new game
+    void newGame();
+
+    //Takes a filename to load a saved game
+    void loadGame();
+};
+
+
+#endif //APT_A2_MENU_H
