1784e8fddfa3a7a8b3944759cc7394a9fed0c94b
Michael Lyberopoulos <s3787260@student.rmit.edu.au>
2020-05-07 20:39:38 +1000

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Menu.h"
 
+
 void Menu::displayWelcome()
 {
     std::cout << "Welcome to Azul!\n" <<
@@ -10,8 +11,9 @@ void Menu::displayWelcome()
 
 void Menu::displayMenu()
 {
+    gameEngine=  make_shared<GameEngine>();
     bool quit = false;
-    while(!quit)
+    while(std::cin.eof() || !quit )
     {
         std::cout << "Menu\n" <<
                   SHORT_LB << "\n" <<
@@ -29,7 +31,7 @@ void Menu::displayMenu()
 
         if(menuSelection == 1)
         {
-            newGame();
+            gameEngine->newGame();
         }
         else if(menuSelection == 2)
         {
@@ -39,12 +41,8 @@ void Menu::displayMenu()
         {
             showCredits();
         }
+         
         else if(menuSelection == 4)
-        {
-            quit = true;
-            std::cout << "Goodbye" << std::endl;
-        }
-        else if(std::cin.eof())
         {
             quit = true;
             std::cout << "Goodbye" << std::endl;
@@ -73,23 +71,6 @@ void Menu::showCredits()
     std::cout << LONG_LB << "\n" << std::endl;
 }
 
-void Menu::newGame()
-{
-    std::string p1Name = "";
-    std::string p2Name = "";
-
-    std::cout << "Starting a New Game\n" << std::endl;
-    std::cout << "Enter a name for player 1" << std::endl;
-    std::cout << PROMPT;
-    std::cin >> p1Name;
-    std::cout << "Enter a name for player 2" << std::endl;
-    std::cout << PROMPT;
-    std::cin >> p2Name;
-
-    std::cout << "\nLet's Play!\n" << std::endl;
-
-    //TODO start game
-}
 
 void Menu::loadGame()
 {
@@ -101,6 +82,7 @@ void Menu::loadGame()
 
     //TODO check that file exists
     //TODO check that file contains a valid game (format of valid game TBC)
+    //TODO load game and continue
 }
 
 
